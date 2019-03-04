b64451a78c0a6a29769737cd6b80ac2518fec554
Laura Felce <crepu83@yahoo.it>
2019-03-04 11:08:08 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -15,6 +15,7 @@ class Show < ActiveRecord::Base
 
   def self.least_popular_show
       Show.order(rating: :asc).first
-  end    
+  #Alternatively => Show.where("rating" = ?, Show.lowest_rating).first
+  end
 
 end
